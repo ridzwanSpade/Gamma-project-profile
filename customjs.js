2d4d06fcedfc951eb013814bdcb751cd48f31dab
aihanif <hanifh82@yahoo.com>
2016-09-24 22:38:14 -0700

    js

---
+++ customjs.js
@@ -0,0 +1,7 @@
+/*
+Plugin Name: custom
+Author: hanif
+
+Example:
+<p data-animate-scroll='{"scaleX": "1.5","scaleY": "1.5","x": "-10","y": "-10","rotation": "-3","alpha": "1","easingType": "Cubic.easeOut","duration": "1"}'>test</p>
+ */
